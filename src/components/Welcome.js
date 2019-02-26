7a67311a30548ff36c208dd9bb7117c35def0e68
2dearsue <2dearsue@gmail.com>
2019-02-26 17:12:26 +0100

    change of colors

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -9,30 +9,30 @@ export default class Welcome extends Component {
       <>
         <div className="Jumbotron">
 
-        <h3 className="display-5 text-center text-dark mt-4 text-uppercase">Book your course here:</h3>
+        <h3 className="display-5 text-center text-secondary mt-4 text-uppercase">Book your course here:</h3>
           <div className="card-deck m-3">
-            <div className="card text-white bg-dark m-3 d-inline-block">
+            <div className="card text-white bg-secondary m-3 d-inline-block">
               <div className="card-body">
                 <h4 className="card-title text-center text-light">Beginner</h4>
                 <img className="card-img" src={Näh1} alt="Card" />
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                <a href="#!" class="card-link text-warning">Sign Up</a>
+                <a href="#!" class="card-link text-dark border border-dark p-2 rounded">Sign Up</a>
               </div>
             </div>
-            <div className="card text-white bg-dark m-3 d-inline-block">
+            <div className="card text-white bg-secondary m-3 d-inline-block">
               <div className="card-body">
                 <h4 className="card-title text-center text-light">Average</h4>
                 <img className="card-img" src={Näh2} alt="Card" />
                 <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <a href="#!" class="card-link text-warning">Sign Up</a>
+                <a href="#!" class="card-link text-dark border border-dark p-2 rounded">Sign Up</a>
               </div>
             </div>
-            <div className="card text-white bg-dark m-3 d-inline-block">
+            <div className="card text-white bg-secondary m-3 d-inline-block">
               <div className="card-body">
                 <h4 className="card-title text-center text-light">Advanced</h4>
                 <img className="card-img" src={Näh3} alt="Card" />
                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                <a href="#!" class="card-link text-warning">Sign Up</a>
+                <a href="#!" class="card-link text-dark border border-dark p-2 rounded">Sign Up</a>
               </div>
             </div>
           </div>
