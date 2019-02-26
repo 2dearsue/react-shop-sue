b3d0a64ec8e5bb3fe1fedb8e53b8e5b616a83b76
2dearsue <2dearsue@gmail.com>
2019-02-26 17:08:08 +0100

    bootstrap production

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import Welcome from './Welcome'
 
 export default class App extends Component {
